29477b6dfd4492e886493e06363ca6629b10a233
yurijGagarin <yurazhagan@gmail.com>
2022-08-15 22:58:41 +0300

    add logout

--- aiogram_django_app/settings.py
+++ aiogram_django_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ["HOST"], '127.0.0.1']
+ALLOWED_HOSTS = [os.environ["HOST"]]
 
 # Application definition
 
