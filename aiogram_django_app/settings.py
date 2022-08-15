9a712f050cd5768305fd3c9cec3cf1b93e830acb
yurijGagarin <yurazhagan@gmail.com>
2022-08-15 18:19:39 +0300

    fix session

--- aiogram_django_app/settings.py
+++ aiogram_django_app/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]]
 
@@ -124,6 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = ['airtable.auth_backend.AuthBackend']
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 AUTH_USER_MODEL = 'airtable.DjangoUser'
